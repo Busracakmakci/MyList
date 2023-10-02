2e94aa3760d25b7f7996bf114bf1e4b3c853a33e
Büşra Çakmakcı <busracakmakci95@gmail.com>
2023-10-02 17:14:27 +0300

    2.10.23

---
+++ Dictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyNamespace
+{
+    class Dictionary 
+    {
+        static void Main ()
+        {
+            Dictionary<string, int> Vegetable = new Dictionary<string, int> ();
+            Vegetable.Add("Banana", 12345);
+            Vegetable.Add("Orange", 236545);
+            Vegetable.Remove("Banana");
+            Vegetable.Add("Cherry", 35678);
+            Vegetable.Clear();
+
+            //Console.WriteLine("Dictionary");
+
+
+            foreach (KeyValuePair<string, int> item in Vegetable)
+            {
+                Console.WriteLine("Vegetable: {0}, Value :{1}", item.Key, item.Value);
+            }
+
+        }
+          
+    }
+}
