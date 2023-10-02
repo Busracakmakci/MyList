f05f09b53578ce2be627f9d8954fb0655662d951
Büşra Çakmakcı <busracakmakci95@gmail.com>
2023-10-02 16:34:17 +0300

    Add project files.

---
+++ HandMadeList/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyList;
+
+namespace MyNamespace
+{
+	class Program
+
+	{
+		static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("busra");
+
+        }
+    }
+}
