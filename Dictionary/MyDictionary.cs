1d293e417521deeeda965598fa394a2ad90cc1f4
Büşra Çakmakcı <busracakmakci95@gmail.com>
2023-10-04 10:26:41 +0300

    04.10.23

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+     class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        TKey[] tempKeys;
+        TValue[] tempValues;
+
+        //constructor
+        public MyDictionary() {
+            
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey item, TValue item2)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            keys=new TKey[keys.Length+1];
+            values=new TValue[values.Length+1];
+
+            for(int i=0; i<keys.Length; i++)
+            {
+                keys[i]=tempKeys[i];
+                values[i]=tempValues[i];
+            }
+            keys[keys.Length-1]=item;
+            values[values.Length-1]=item2;
+
+             Console.WriteLine("Vegetable: {0}, Value :{1}", item, item2);
+        }
+
+     }
+}
--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -9,22 +9,43 @@ namespace MyNamespace
 {
     class Dictionary 
     {
+        class MyDictionary<TKey, TValue>
+        {
+            private List<KeyValuePair<TKey, TValue>> items;
+
+            public MyDictionary()
+            {
+                items = new List<KeyValuePair<TKey, TValue>>();
+            }
+
+            public void Add(TKey key, TValue value)
+            {
+                items.Add(new KeyValuePair<TKey, TValue>(key, value));
+            }
+
+            public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+            {
+                return items.GetEnumerator();
+            }
+        }
         static void Main ()
         {
-            Dictionary<string, int> Vegetable = new Dictionary<string, int> ();
-            Vegetable.Add("Banana", 12345);
-            Vegetable.Add("Orange", 236545);
-            Vegetable.Remove("Banana");
-            Vegetable.Add("Cherry", 35678);
-            Vegetable.Clear();
+            MyDictionary<string, int> vegetableDictionary = new MyDictionary<string, int> ();
+            vegetableDictionary.Add("Banana", 12345);
+            vegetableDictionary.Add("Orange", 236545);
+          
+            vegetableDictionary.Add("Cherry", 35678);
+            
 
-            //Console.WriteLine("Dictionary");
+           
 
-
-            foreach (KeyValuePair<string, int> item in Vegetable)
+            foreach (KeyValuePair<string, int> item in vegetableDictionary)
             {
                 Console.WriteLine("Vegetable: {0}, Value :{1}", item.Key, item.Value);
             }
+
+
+            //KeyValuePair string int şeklinde yazdırır. Karışıklığı önler. "var" da yazabilirsin.
 
         }
           
